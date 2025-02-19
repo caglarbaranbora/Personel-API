668b9dfd1558c98fb0d0dbb9a90bbf7e52558364
caglarbaranbora <boracaglarbaran@gmail.com>
2025-02-19 18:15:29 +0300

    reupdated

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(
+  "mongodb+srv://caglarbaranbora:" +
+    process.env.MONGO_ATLAS_PW +
+    "@personal-api.trtf9.mongodb.net/?retryWrites=true&w=majority&appName=personal-api"
+);
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.header("Access-Control-Allow-Origin", "*"); //any domain can access but we can change it to the exact domain
+  req.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+//Routes which should handle requests
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
